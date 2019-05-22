cdd855bb6963d0f844764b96e00ed89e158f4cc5
Blake Mason <bmason@lsst.org>
2019-05-22 12:31:49 -0700

    [C] Add favicon
    Resolves #6
    
    Uses webpack to create a favicon from a given image, and then injects those assets and relevant meta data into the head;
    Does not include advanced configuration of metadata
    
    EPO-1837

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,6 +4,7 @@ const history = require('connect-history-api-fallback');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const AppManifestWebpackPlugin = require('app-manifest-webpack-plugin');
 const commonPaths = require('./paths');
 
 module.exports = {
@@ -69,6 +70,42 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: commonPaths.templatePath,
+    }),
+    new AppManifestWebpackPlugin({
+      logo: './src/assets/images/lsst_logo_small.png',
+      persistentCache: true,
+      output: './',
+      config: {
+        appName: 'LSST App', // Your application's name. `string`
+        appDescription: null, // Your application's description. `string`
+        developerName: null, // Your (or your developer's) name. `string`
+        developerURL: null, // Your (or your developer's) URL. `string`
+        background: '#ffffff', // Background colour for flattened icons. `string`
+        theme_color: '#ffffff', // Theme color for browser chrome. `string`
+        display: 'standalone', // Android display: "browser" or "standalone". `string`
+        orientation: 'portrait', // Android orientation: "portrait" or "landscape". `string`
+        start_url: '/?homescreen=1', // Android start application's URL. `string`
+        version: '1.0.0', // Your application's version number. `number`
+        logging: false, // Print logs to console? `boolean`
+        icons: {
+          // Platform Options:
+          // - offset - offset in percentage
+          // - shadow - drop shadow for Android icons, available online only
+          // - background:
+          //   * false - use default
+          //   * true - force use default, e.g. set background for Android icons
+          //   * color - set background for the specified icons
+          //
+          android: true, // Create Android homescreen icon. `boolean` or `{ offset, background, shadow }`
+          appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background }`
+          appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background }`
+          coast: { offset: 25 }, // Create Opera Coast icon with offset 25%. `boolean` or `{ offset, background }`
+          favicons: true, // Create regular favicons. `boolean`
+          firefox: true, // Create Firefox OS icons. `boolean` or `{ offset, background }`
+          windows: true, // Create Windows 8 tile icons. `boolean` or `{ background }`
+          yandex: true, // Create Yandex browser icon. `boolean` or `{ background }`
+        },
+      },
     }),
     new ScriptExtHtmlWebpackPlugin({
       defaultAttribute: 'async',
